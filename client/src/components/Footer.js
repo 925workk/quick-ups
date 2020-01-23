78b53ff4852a844b5d771dd845af68741234dde4
Nelson Knight <nelson.knight92@gmail.com>
2020-01-22 18:11:53 -0700

    fixing footer container

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,13 +3,15 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
+                <div className="card-footer">
                 <div className="container">
                 <a className="navbar-brand" href="/">QuickUps</a>
                 <ul className="nav navbar-nav flex-row float-left">
                     <li className="footer-item"><a className="footer-link pr-4 active" href="/">Home</a></li>
                     <li className="footer-item"><a className="footer-link pr-4" href="https://github.com/925work/quick-ups">GitHub Repo</a></li>
-                    <li className="footer-item"><a className="footer-link pr-4" href="saved">Login</a></li>
+                    <li className="footer-item"><a className="footer-link pr-4">Copyright 2020 Alex, Charissa, Myles and Nelson</a></li>
                 </ul>
+                </div>
                 </div>
         </div>
     )

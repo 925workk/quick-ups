9bba7ca6d7d2d4b44433a35f3eea666179b040a2
Nelson Knight <nelson.knight92@gmail.com>
2020-01-22 18:17:08 -0700

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/Home"
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Home />
+      </div>
+      <div>
+        <Footer />
       </div>
     );
   }
